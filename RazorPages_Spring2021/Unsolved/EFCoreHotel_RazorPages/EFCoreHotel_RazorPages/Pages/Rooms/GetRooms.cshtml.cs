58f9d67fedff3a6b510919f161d8672f9bd52c76
Mohammed El Allali <moal@easj.dk>
2021-02-21 14:20:52 +0100

    add hotel project

---
+++ RazorPages_Spring2021/Unsolved/EFCoreHotel_RazorPages/EFCoreHotel_RazorPages/Pages/Rooms/GetRooms.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreHotel_RazorPages.HotelDBContext;
+using EFCoreHotel_RazorPages.Models;
+using EFCoreHotel_RazorPages.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EFCoreHotel_RazorPages
+{
+    public class GetRoomsModel : PageModel
+    {
+          public IEnumerable<Room> Rooms { get; set; }
+       
+         IRoomService  roomService;
+        public GetRoomsModel(IRoomService service)
+        {
+           roomService= service;
+        }
+        public void OnGet()
+        {
+             Rooms = roomService.GetRooms();
+        }
+    }
+}
