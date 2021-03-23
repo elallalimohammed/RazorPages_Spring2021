51c046afdc2f48c0071e95643419bc52b87c2575
Mohammed El Allali <moal@easj.dk>
2021-03-23 23:21:38 +0100

    test

--- RazorPages_Spring2021/Program.cs
+++ RazorPages_Spring2021/Program.cs
@@ -15,7 +15,6 @@ namespace RazorPages_Spring2021
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
